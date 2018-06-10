140ab1038ca5711132365435b97dc903ab343007
donrestarone <shashikejayatunge@gmail.com>
2018-06-10 12:40:53 -0400

    templating and index view works

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // get express + bodyparser
 const express = require('express');
 const bodyParser = require('body-parser');
+// production/test environment port setup
+const port = process.env.PORT || 3000;
+
 // connect to mongoDB, authenticate, and create a db object
 var db
 var MongoUsr = process.env.TODO_MONGO_USR;
@@ -9,7 +12,7 @@ const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(`mongodb://${MongoUsr}:${MongoPass}@ds153980.mlab.com:53980/to-dos`, (err, database) => {
     if (err) return console.log(err);
     db = database.db('to-dos');
-    app.listen(3000, function(){
+    app.listen(port, function(){
         console.log('listening on port 3000');
     });
 });
@@ -17,13 +20,20 @@ MongoClient.connect(`mongodb://${MongoUsr}:${MongoPass}@ds153980.mlab.com:53980/
 const app = express();
 // invoke body parser for the app. here urlencoded method will enable you to access body parser date in the body property of the request object
 app.use(bodyParser.urlencoded({extended: true}));
-// app is listening on port 3000, when deployed use ENV variable here instead
-
+// invoke ejs/nunjucks as the view engine for templates
+app.set('view engine', 'ejs');
 
 // root path, will be using newer ES6 syntax after this.
 app.get('/', function(req, res) {
-    // res.sendFile('root path works');
-    res.sendFile(__dirname + '/index.html')
+    // get the collection of 'todos' from mongolab, disregard the irrelevant stuff and return the todos in an array
+    var cursor = db.collection('todos').find().toArray(function(err, result){
+        console.log(result);
+        res.render('index.ejs', {todos: result});
+    });
+
+    
+    // res.sendFile('root path works')
+    // res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/todos', (req, res) => {
