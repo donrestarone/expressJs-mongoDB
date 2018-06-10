8a322b2d52d2be2515695a502e2e47932f538ca2
donrestarone <shashikejayatunge@gmail.com>
2018-06-10 11:42:06 -0400

    set up bodyparser, formfield for post request. now ready for mongodb

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// get express + bodyparser
 const express = require('express');
+const bodyParser = require('body-parser');
+// create instance of express
 const app = express();
-
+// invoke body parser for the app. here urlencoded method will enable you to access body parser date in the body property of the request object
+app.use(bodyParser.urlencoded({extended: true}));
 // app is listening on port 3000, when deployed use ENV variable here instead
 app.listen(3000, function(){
     console.log('listening on port 3000');
@@ -10,5 +14,9 @@ app.listen(3000, function(){
 app.get('/', function(req, res) {
     // res.sendFile('root path works');
     res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
 });
 
